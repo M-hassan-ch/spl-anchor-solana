fdff0a9b4faaaa383bb6dd828d76032e42d91f6a
Hassan <muhammad.hassan@antematter.io>
2025-04-17 19:34:12 +0500

    added instruct to transfer tokens with  unit tests

--- programs/spl/src/lib.rs
+++ programs/spl/src/lib.rs
@@ -8,17 +8,20 @@ declare_id!("HwV15MoQC8MdpeLpp9seYtTFBVTF19zYJCnSMQc9b5au");
 
 #[program]
 pub mod spl {
-    use anchor_spl::token::{mint_to, MintTo};
+    use anchor_spl::token::{mint_to, transfer, MintTo, Transfer};
 
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Initializing mint....");
         msg!("Token program: {:?}", ctx.accounts.token_program.key());
-        msg!("Associated token program: {:?}", ctx.accounts.associated_token_program.key());
+        msg!(
+            "Associated token program: {:?}",
+            ctx.accounts.associated_token_program.key()
+        );
 
         let signer_key = &ctx.accounts.signer.key();
-        let signer_keys: &[&[&[u8]]] = &[&[b"vault", signer_key.as_ref(), &[ctx.bumps.vault]]];
+        let signer_seeds: &[&[&[u8]]] = &[&[b"vault", signer_key.as_ref(), &[ctx.bumps.vault]]];
         let vault = &mut ctx.accounts.vault;
         vault.creator = ctx.accounts.signer.key();
         vault.bump = ctx.bumps.vault;
@@ -30,11 +33,32 @@ pub mod spl {
                 mint: ctx.accounts.new_mint.to_account_info(),
                 to: ctx.accounts.token_account.to_account_info(),
             },
-            signer_keys,
+            signer_seeds,
         );
 
         let _ = mint_to(cpi_context, 1_000)?;
 
+        Ok(())
+    }
+
+    pub fn transfer_tokens(ctx: Context<Grab>, amount: u64) -> Result<()> {
+        msg!("Transferring {:?} from vault to signer", amount);
+
+        let signer_key = &ctx.accounts.vault.creator;
+        let signer_seeds: &[&[&[u8]]] =
+            &[&[b"vault", signer_key.as_ref(), &[ctx.accounts.vault.bump]]];
+
+        let cpi_context = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            Transfer {
+                from: ctx.accounts.from_vault_ata.to_account_info(),
+                to: ctx.accounts.to_vault_ata.to_account_info(),
+                authority: ctx.accounts.vault.to_account_info(),
+            },
+            signer_seeds,
+        );
+
+        let _ = transfer(cpi_context, amount)?;
         Ok(())
     }
 }
@@ -74,6 +98,43 @@ pub struct Initialize<'info> {
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+#[derive(Accounts)]
+pub struct Grab<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        seeds = [b"mint", vault.creator.as_ref()],
+        bump,
+        mint::authority = vault,
+        mint::decimals = 0,
+    )]
+    pub mint: Account<'info, Mint>,
+
+    #[account(
+        seeds = [b"vault", vault.creator.as_ref()],
+        bump = vault.bump,
+    )]
+    pub vault: Account<'info, Vault>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = vault,
+    )]
+    pub from_vault_ata: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = signer,
+    )]
+    pub to_vault_ata: Account<'info, TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
 }
 
 #[account]
--- tests/spl.ts
+++ tests/spl.ts
@@ -1,16 +1,45 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { BN, Program } from "@coral-xyz/anchor";
 import { Spl } from "../target/types/spl";
 import * as splToken from "@solana/spl-token";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
-async function airDrop(program: Program<Spl>, signer: anchor.web3.Keypair) {
-  const tx = await program.methods.initialize().rpc();
-  console.log("Your airdrop transaction signature", tx);
+async function airdrop(connection: anchor.web3.Connection, payer: anchor.web3.Keypair, amount: number) {
+  const signature = await connection.requestAirdrop(
+    payer.publicKey,
+    amount * LAMPORTS_PER_SOL
+  );
+  let latestBlockhash = await connection.getLatestBlockhash();
+  return await connection.confirmTransaction({
+    signature: signature,
+    ...latestBlockhash
+  });
 }
+
+/**
+ * Utility function to deserialize data of a given associated token account address
+ * @param connection The Solana connection
+ * @param tokenAccountAddress The address of the token account to deserialize
+ * @returns The deserialized token account data
+ */
+async function getTokenAccountData(
+  connection: anchor.web3.Connection,
+  tokenAccountAddress: anchor.web3.PublicKey
+): Promise<splToken.Account> {
+  const tokenAccountInfo = await connection.getAccountInfo(tokenAccountAddress);
+
+  if (!tokenAccountInfo) {
+    throw new Error(`Token account ${tokenAccountAddress.toString()} not found`);
+  }
+
+  return splToken.unpackAccount(tokenAccountAddress, tokenAccountInfo);
+}
+
 
 describe("spl", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Spl as Program<Spl>;
 
@@ -24,12 +53,46 @@ describe("spl", () => {
     [Buffer.from("vault"), signer.toBuffer()],
     program.programId
   );
-  const tokenAccountPDA = splToken.getAssociatedTokenAddressSync(mintPDA, vaultPDA, true);
+
+  function getProgramWithProvider(provider: anchor.AnchorProvider): anchor.Program {
+    return new anchor.Program(program.idl as anchor.Idl, provider);
+  }
 
   it("Is initialized!", async () => {
     const tx = await program.methods.initialize()
-    .accounts({})
-    .rpc({commitment:"confirmed"});
+      .accounts({})
+      .rpc({ commitment: "confirmed" });
     console.log("Your transaction signature", tx);
+  });
+
+  it("Transfer tokens", async () => {
+    const signer2 = anchor.web3.Keypair.generate();
+    await airdrop(provider.connection, signer2, 100);
+    const _provider = new anchor.AnchorProvider(
+      provider.connection,
+      new anchor.Wallet(signer2),
+      {}
+    );
+    const _program = getProgramWithProvider(_provider);
+
+    const signer2_ata_address = await splToken.getAssociatedTokenAddress(mintPDA, signer2.publicKey, true);
+    const signer1_ata_address = splToken.getAssociatedTokenAddressSync(mintPDA, vaultPDA, true);
+
+    await splToken.createAssociatedTokenAccount(_provider.connection, signer2, mintPDA, signer2.publicKey, { commitment: 'confirmed' })
+
+    console.log("Sender ATA Before: ", ((await getTokenAccountData(_provider.connection, signer1_ata_address)).amount));
+    console.log("Received ATA Before: ", ((await getTokenAccountData(_provider.connection, signer2_ata_address)).amount));
+
+    const tx = await _program.methods.transferTokens(new BN(10))
+      .accounts({
+        signer: signer2,
+        vault: vaultPDA,
+      })
+      .signers([signer2])
+      .rpc({ commitment: "confirmed" });
+
+    console.log("Your transaction signature", tx);
+    console.log("Sender ATA After: ", ((await getTokenAccountData(_provider.connection, signer1_ata_address)).amount));
+    console.log("Received ATA After: ", ((await getTokenAccountData(_provider.connection, signer2_ata_address)).amount));
   });
 });
